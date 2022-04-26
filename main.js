0113a4d76f74cf13e2dd8b996c851d8d74a9d78d
Yandiswa Sompali <yandiswasompali@gmail.com>
2022-04-26 15:22:02 +0200

    adding index

---
+++ main.js
@@ -0,0 +1,7 @@
+//Reverse string Chain Method
+
+function reverseString(s){
+    return s.split("").reverse().join("");// after spliting returns arrey, reverses the arrey the joins the reverse arreys
+}
+console.log(reverseString("yandiswa")); 
+
---
+++ script.js
@@ -0,0 +1,9 @@
+// + - * / %
+function cToF(celsius) // 
+{
+  return(celcius * 9 / 5 + 32))
+}
+
+console.log(celcius(32))
+
+
